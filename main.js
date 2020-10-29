6657df1b491dd0f574be232d4a778a2399bafaf9
Rizal Asrul Pambudi <rizal.asrul@live.com>
2020-10-29 22:52:44 +0700

    feat: Add pengenalan

---
+++ main.js
@@ -0,0 +1,9 @@
+console.log('Pernyataan 1');
+
+setTimeout(function() {
+  console.log('Callback function berjalan');
+}, 2000);
+
+console.log('Pernyataan 2');
+console.log('Pernyataan 3');
+console.log('Pernyataan 4');
